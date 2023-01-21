85fee554a9f63c9382c98d18e658f814994c5965
Gabriel Marcolino Silva <66142528+Gabriel-MarcoSilva@users.noreply.github.com>
2023-01-21 17:10:47 -0300

    feat: criação e utiização dos dados

--- src/app/card-pokemon/card-pokemon.component.ts
+++ src/app/card-pokemon/card-pokemon.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from "../../_models/Pokemon"
 
 @Component({
   selector: 'app-card-pokemon',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-pokemon.component.css']
 })
 export class CardPokemonComponent {
-
+  @Input()
+  public pokemon!: Pokemon;
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pokemon } from "../../_models/Pokemon"
+import { Type } from "../../_models/Type"
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.css']
 })
 export class PokemonListComponent {
+
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: "Bulbasaur",
+      type: [
+        Type.Grass, Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: "Ivysaur",
+      type: [
+        Type.Grass, Type.Poison
+      ]
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: "Venosaur",
+      type: [
+        Type.Grass, Type.Poison
+      ]
+    }
+  ]
 
 }
