d1658abe3dc0a281f4a66210f2f2aea99a534ed8
Gabriel Marcolino Silva <66142528+Gabriel-MarcoSilva@users.noreply.github.com>
2023-01-27 17:52:22 -0300

    chore: limpando paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header.component';
-import { Aula3Component } from './components/aula3/aula3.component';
-import { Aula4Component } from './components/aula4/aula4.component';
-import { Aula5Component } from './components/aula5/aula5.component';
-import { Aula6Component } from './components/aula6/aula6.component';
-import { Aula7Component } from './components/aula7/aula7.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    Aula3Component,
-    Aula4Component,
-    Aula5Component,
-    Aula6Component,
-    Aula7Component
   ],
   imports: [
     BrowserModule,
