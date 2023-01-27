a94cbfdb4bbc6c0f332457062283c2d382f78baf
Gabriel Marcolino Silva <66142528+Gabriel-MarcoSilva@users.noreply.github.com>
2023-01-27 16:47:07 -0300

    feat: mandando uma propriedade pra outro componente (hierarquia)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { Aula3Component } from './components/aula3/aula3.component';
 import { Aula4Component } from './components/aula4/aula4.component';
 import { Aula5Component } from './components/aula5/aula5.component';
+import { Aula6Component } from './components/aula6/aula6.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { Aula5Component } from './components/aula5/aula5.component';
     HeaderComponent,
     Aula3Component,
     Aula4Component,
-    Aula5Component
+    Aula5Component,
+    Aula6Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/aula6/aula6.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aula6',
+  templateUrl: './aula6.component.html',
+  styleUrls: ['./aula6.component.css']
+})
+export class Aula6Component {
+
+}
