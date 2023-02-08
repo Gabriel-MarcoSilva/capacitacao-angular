91fe5d85a515e0b32e7c5dfd815f345f87c1db23
Gabriel Marcolino Silva <66142528+Gabriel-MarcoSilva@users.noreply.github.com>
2023-02-08 10:08:10 -0300

    chore: primeiro commit, restart dos arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from "./components/login/login.component"
 import { AuthGuardService } from './resources/services/auth-guard.service';
 
 const routes: Routes = [
  // {path: '', recirectTo: 'arquivo'}
-  {path: '', component: LoginComponent},
-  {path:
+  /*{path:
     'dashboard',
     canActivate: [AuthGuardService],
     loadChildren:() => import('./components/dashboard/dashboard.module').then((m) => m.DashboardModule)},
   //pra não carregar todos os modulos de vez; carrega os modulos sobre demanda
-  {path:"**", redirectTo: ""}
+  {path:"**", redirectTo: ""}*/
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardModule } from 'primeng/card';
-import { LoginComponent } from './components/login/login.component';
-import { InputTextModule } from "primeng/inputtext"
-import { ButtonModule } from "primeng/button"
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,9 +17,6 @@ import { ButtonModule } from "primeng/button"
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    CardModule,
-    InputTextModule,
-    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
